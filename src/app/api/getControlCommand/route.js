539f8b9bcd078035f047c4a73bac2297a9ce6f22
Kurozx <natchanondokchan@gmail.com>
2024-09-02 23:54:57 +0700

    E

--- src/app/api/getControlCommand/route.js
+++ src/app/api/getControlCommand/route.js
@@ -21,7 +21,7 @@ export async function POST(request) {
 
       const res = await client.query(
           'UPDATE "NCN046" SET command = $1 WHERE id = $2 RETURNING *',
-          [command, 87] // ใช้ `1` เป็น ID ของแถวที่ต้องการอัปเดต หากมีหลายแถวให้ปรับเป็น ID ที่ต้องการ
+          [command, 1] // ใช้ `1` เป็น ID ของแถวที่ต้องการอัปเดต หากมีหลายแถวให้ปรับเป็น ID ที่ต้องการ
       );
 
       if (res.rowCount === 0) {
@@ -44,7 +44,7 @@ export async function POST(request) {
 export async function GET() {
   try {
     // ดึงข้อมูลสถานะปัจจุบันจากฐานข้อมูล
-    const res = await client.query('SELECT command FROM "NCN046" WHERE id = $1', [87]);
+    const res = await client.query('SELECT command FROM "NCN046" WHERE id = $1', [1]);
 
     if (res.rowCount === 0) {
       throw new Error('No records found');
