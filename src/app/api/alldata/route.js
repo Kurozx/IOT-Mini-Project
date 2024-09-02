d686a317361ae00b881ef65dfaf3ee4bbc759a1d
Kurozx <natchanondokchan@gmail.com>
2024-09-02 13:21:52 +0700

    r

--- src/app/api/alldata/route.js
+++ src/app/api/alldata/route.js
@@ -29,7 +29,7 @@ export async function GET() {
   } catch (error) {
     console.error('GET Error:', error.stack || error.message);
     return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
+      status: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
@@ -68,7 +68,7 @@ export async function POST(request) {
   } catch (error) {
     console.error('POST Error:', error.stack || error.message);
     return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
+      status: 201,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
