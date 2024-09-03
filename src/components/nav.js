c28dd7fd123343459c69097ec602498ba05232e9
Kurozx <natchanondokchan@gmail.com>
2024-09-03 09:51:24 +0700

    EEE

--- src/components/nav.js
+++ src/components/nav.js
@@ -73,10 +73,8 @@ const Navbar = () => {
             </li>
           </ul>
           <form className="d-flex align-items-center">
-            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('RGB_ON', setLEDStatus)}>RGB ON</button>
-            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('BUZZER_ON', setLEDStatus)}>On Buzzer</button>
-            <button type="button" className={`btn ${styles.btnDanger}`} onClick={() => updateLEDStatus('OFF', setLEDStatus)}>Off RGB And Buzzer</button>
-            
+            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('RGB_ON', setLEDStatus)}>RGB AND Buzzer on</button>
+            <button type="button" className={`btn ${styles.btnDanger}`} onClick={() => updateLEDStatus('OFF', setLEDStatus)}>Off RGB And Buzzer</button>            
           </form>
         </div>
       </div>
